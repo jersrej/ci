f307536e7ea60cc101ce214bf3193b1ba249e2f1
jersrej <jerson.conmigo@gmail.com>
2016-07-26 17:27:24 +0800

    sku pull
    added submit button

--- application/views/skupull.php
+++ application/views/skupull.php
@@ -5,9 +5,8 @@
 	<?php
 		$attributes = array('class' => 'form_skus', 'id' => 'form_skus', 'name' => 'form_skus');
 		echo form_open('pull/validation', $attributes);
-		
-		//echo form_textarea()
 		echo form_textarea(array('name' => 'text_skus', 'autocomplete' => 'off', 'class' => 'text_skus'));
+		echo form_submit(array('name' => 'submit', 'class' => 'submit_button'));
 	?>	
 </body>
 </html>
