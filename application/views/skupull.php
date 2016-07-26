807f48c09825f92d38b23cb07452eaa1b386f4ef
jersrej <jerson.conmigo@gmail.com>
2016-07-26 18:04:34 +0800

    sku pull

--- application/views/skupull.php
+++ application/views/skupull.php
@@ -4,9 +4,9 @@
 	<p>Sku pull</p>
 	<?php
 		$attributes = array('class' => 'form_skus', 'id' => 'form_skus', 'name' => 'form_skus');
-		echo site_url('pull/validation', $attributes);
+		echo form_open('pull/validation', $attributes);
 		echo form_textarea(array('name' => 'text_skus', 'autocomplete' => 'off', 'class' => 'text_skus'));
-		echo form_submit(array('name' => 'submit', 'class' => 'submit_button'));
+		echo form_submit(array('name' => 'submit', 'class' => 'submit_button', 'value' => 'submit'));
 	?>	
 </body>
 </html>
